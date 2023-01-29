886bd7aeee0e263a0128d96a4a2dfed10d89e812
Natalie R <tatysia@gmail.com>
2023-01-30 02:09:41 +0300

    Задача 3 о таблице кубов от 1 до N

---
+++ Ex3_Cube_N/Program.cs
@@ -0,0 +1,14 @@
+﻿// Ввести число N и вывести таблицу кубов от 1 до N.
+
+void CubeTable(int x)
+{
+    for (int i = 1; i<=x; i++)
+    {
+        Console.WriteLine(i*i*i);
+    }
+}
+
+Console.WriteLine("Введите N: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine();
+CubeTable(number);
