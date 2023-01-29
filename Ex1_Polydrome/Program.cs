e7b6c9f1aa74774d46f7d3f4530ef78df01a0435
Natalie R <tatysia@gmail.com>
2023-01-30 02:53:11 +0300

    Задача 1 о палиндроме

---
+++ Ex1_Polydrome/Program.cs
@@ -0,0 +1,21 @@
+﻿// Ввести пятизначное число. Проверить, является ли оно палиндромом.
+
+void Palindrome(int n)
+{
+    int x1 = n / 10000;
+    int x5 = n % 10;
+    int x2 = n / 1000 % 10;
+    int x4 = n / 10 % 10;
+    if ((x1 == x5) && (x2 == x4))
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+Console.WriteLine("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Palindrome(number);
